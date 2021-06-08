e6f85bbcf29ed75e392775eb295c5f055e1844be
Md Rehan <rehanme125@gmail.com>
2021-06-08 19:38:34 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { ServiceRequest } from "./components/ServiceRequest";
 import { EnquiryList } from "./components/EnquiryList";
 import { ServiceRequestList } from "./components/ServiceRequestList";
 import { VehicleList } from "./components/VehicleList";
+import { ServiceDetail } from "./components/ServiceDetail";
 function App() {
   return (
     <div>
@@ -54,6 +55,9 @@ function App() {
         </Route>
         <Route path="/vehiclelist">
           <VehicleList />
+        </Route>
+        <Route path="/servicedetails">
+          <ServiceDetail />
         </Route>
       </Router>
     </div>
