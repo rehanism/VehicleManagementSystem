678eb775683659ae0b879dcff890b5849cdc6526
Md Rehan <rehanme125@gmail.com>
2021-06-09 20:29:31 +0530

    Sprint 2

--- src/common/AppNavBar.js
+++ src/common/AppNavBar.js
@@ -10,29 +10,57 @@ export function AppNavBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/login">
-            Login
-          </Nav.Link>
           <Nav.Link as={Link} to="/home">
             Home
           </Nav.Link>
+          <Nav.Link as={Link} to="/login">
+            Login
+          </Nav.Link>
           <Nav.Link as={Link} to="/user-register">
-            User
-          </Nav.Link>
-          <Nav.Link as={Link} to="/mechanic">
-            Mechanic
-          </Nav.Link>
-          <Nav.Link as={Link} to="/vehicle">
-            Vehicle
+            Sign-Up or Register
           </Nav.Link>
           <Nav.Link as={Link} to="/enquiry">
             Enquiry
           </Nav.Link>
-          <Nav.Link as={Link} to="/servicerequest">
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+export function UserNavBar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        VEHICLE SERVICE MANAGEMENT
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/service request">
             Service Request
           </Nav.Link>
           <Nav.Link as={Link} to="/servicedetails">
             Service Details
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+export function adminNavbar() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar.Brand as={Link} to="/">
+        VEHICLE SERVICE MANAGEMENT
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/vehicle">
+            Vehicle
+          </Nav.Link>
+          <Nav.Link as={Link} to="/mechanic">
+            Mechanics
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
