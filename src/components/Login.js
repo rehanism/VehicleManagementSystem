d469441a71fda904b4e0e5aac33316cce3d415d4
Md Rehan <rehanme125@gmail.com>
2021-06-10 13:06:31 +0530

    Sprint2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,42 @@
-export function Login() {
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { AppNavBar } from "../common/AppNavBar";
+import { LoginAction } from "../redux/LoginReducer";
+export const Login = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const state = useSelector((state) => state);
+
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const [successOperation, setSuccessOperation] = useState(false);
+  const [errorOperation, setErrorOperation] = useState(false);
+
+  const updateUserEmail = (e) => setUserEmail(e.target.value);
+  const updateUserPassword = (e) => setUserPassword(e.target.value);
+
+  const UserLoginFunction = () => {
+    dispatch(
+      LoginAction({
+        userEmail,
+        userPassword,
+      })
+    );
+    setErrorOperation(true);
+    setTimeout(() => setErrorOperation(false), 5000);
+
+    console.log(userEmail, userPassword);
+    console.log(state.userEmail, state.userPassword);
+  };
+
+  if (state.UserLogin.loginAction === true) {
+    history.push("/user");
+
+    return <div></div>;
+  }
+
   return (
     <div className="row" style={{ marginTop: "30px" }}>
       <div className="col-3 col-md-3 d-none d-md-block"></div>
@@ -6,15 +44,18 @@ export function Login() {
         <h3 class=" border border-primary p-3 mb-2 bg-dark text-primary">
           Log In
         </h3>
+        {state.UserLogin.loginAction === false && errorOperation && (
+          <div className="alert alert-danger">login failure</div>
+        )}
         <div>
           <form>
             <div className="mb-1">
               <input
-                type="username"
-                name="username"
+                type="text"
                 className="form-control"
-                placeholder="username..."
-                required
+                placeholder="Email..."
+                value={userEmail}
+                onChange={(e) => updateUserEmail(e)}
               />
             </div>
             <div className="mb-1">
@@ -23,11 +64,16 @@ export function Login() {
                 name="password"
                 className="form-control"
                 placeholder="password..."
-                required
+                value={userPassword}
+                onChange={(e) => updateUserPassword(e)}
               />
             </div>
             <div className="mb-1">
-              <button className="form-control btn btn-outline-primary">
+              <button
+                className="form-control btn btn-outline-primary"
+                value="Login"
+                onClick={() => UserLoginFunction()}
+              >
                 Log In
               </button>
             </div>
@@ -37,4 +83,4 @@ export function Login() {
       <div className="col-3 col-md-3  d-none d-md-block"></div>
     </div>
   );
-}
+};
